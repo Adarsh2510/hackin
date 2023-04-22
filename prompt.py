db2efd8e66d5f4e3078f53889faa960012c0575e
Heena Singh <heenareenasingh234@gmail.com>
2023-04-22 23:20:16 +0530

    feat: badge model

---
+++ prompt.py
@@ -0,0 +1,15 @@
+prompt_venues = {
+    "value1": "disneyland paris",
+    "value2": "Eiffel Tower",
+    "value3": "Taj Mahal" 
+}
+
+example_prompt = [
+    "place [v] person with the Taj Mahal in the background, with a subtle Bokeh effect that adds depth and dimension to the scene. The final result should be a stunning picture that captures both the beauty of the [v] person and the iconic landmark in the background.",
+    "Image of [v] person posing in front of the Taj Mahal",
+    "beautiful painting of [v] person standing in front of Taj Mahal",
+    "painting of [v] person with beautiful face and the Taj Mahal in the background wearing white gown",
+    "painting of [v] person with Indian outfit posing in front of the Taj Mahal",
+    "painting of [v] person posing in front of Taj Mahal in black suit",
+    "8K resolution, natural lighing, clear sky, sublte bokeh effect for the background,side light should be used to cast dramatic shadows across the subject's face, highlighting their unique features and adding a cinematic touch",
+]
--- replicate_ai_models.py
+++ replicate_ai_models.py
@@ -1,17 +1,17 @@
 import replicate
-import requests
-def enhanceImage (prompt):
+import os
+
+def enhanceImage (input_image_path):
     outputUrl = replicate.run(
-    "nightmareai/real-esrgan:42fed1c4974146d4d2414e2be2c5277c7fcf05fcc3a73abf41610695738c1d7b",
-    input={"image": open("images/test_4.jpg", "rb")}
+    os.getenv('ENHANCE_MODEL'),
+    input={"image": open(input_image_path, "rb")}
     )
     return outputUrl
 
 def createBadge(prompt):
     # badge code here
-    print("here")
     output = replicate.run(
-        "adarsh2510/badgevarun:9fe1029bcbf0c87337b9d4ae9685823c4a675c0d0b9a5af90594459637eac55a",
+        os.getenv('PRE_TRAINED_EDIT_IMAGE_MODEL'),
         input={"prompt": prompt}
     )
     print(output)
